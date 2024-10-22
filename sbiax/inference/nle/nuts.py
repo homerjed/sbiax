c94c633e8fc6489ae3ecadc13cf9765823d94a14
homerjed <jedhmr@gmail.com>
2024-10-22 11:50:57 +0200

    docstring, paper

--- sbiax/inference/nle/nuts.py
+++ sbiax/inference/nle/nuts.py
@@ -1,11 +1,65 @@
+from typing import Tuple, Callable
 import blackjax.progress_bar
 import jax
 import jax.random as jr
 import jax.numpy as jnp
+from jaxtyping import PRNGKeyArray, Array
 import blackjax
+from tensorflow_probability.substrates.jax.distributions import Distribution
 
+def nuts_sample(
+    key: PRNGKeyArray, 
+    log_prob_fn: Callable, 
+    prior: Distribution, 
+    n_samples: int = 100_000
+) -> Tuple[Array, Array]:
+    """
+        Runs NUTS (No-U-Turn Sampler) to sample from a posterior distribution using JAX.
 
-def nuts_sample(key, log_prob_fn, prior=None, n_samples=100_000):
+        This function performs sampling using the NUTS algorithm, implemented via BlackJAX, 
+        with an initial warm-up phase for tuning parameters. It uses the window adaptation 
+        process to adjust the parameters during warm-up and runs the sampler in parallel 
+        for multiple chains.
+
+        Args:
+            key: A JAX `PRNGKeyArray`.
+            log_prob_fn: A callable representing the log probability function of the 
+                posterior distribution. This function should take a set of parameters 
+                as input and return their log probability (`Callable`).
+            prior: A `tensorflow_probability` `Distribution` object representing the 
+                prior distribution from which the initial parameter values are sampled (`Distribution`).
+            n_samples: The number of posterior samples to generate (`int`). Default is 100,000.
+
+        Returns:
+            Tuple[`Array`, `Array`]:
+                - The first array contains the sampled parameter positions from the NUTS algorithm 
+                with shape `(n_samples,)` for one chain.
+                - The second array contains the log densities (log posterior probabilities) corresponding 
+                to each sampled position with shape `(n_samples,)`.
+
+        Process:
+            1. The prior distribution is used to sample initial parameter values.
+            2. The NUTS sampler is tuned and adapted during the warm-up phase using `blackjax.window_adaptation`.
+            3. After warm-up, the function performs sampling over `n_samples` using the NUTS kernel 
+            provided by `blackjax.nuts.build_kernel`, running the sampler for one or more chains.
+            4. The function returns the positions (sampled parameter values) and their associated 
+            log densities from the posterior distribution.
+
+        Example:
+            ```python
+            import jax
+            import blackjax
+            from tensorflow_probability.substrates.jax.distributions import Normal
+
+            def log_prob_fn(params):
+                # Typically this takes in a datavector of some kind!
+                return -0.5 * jnp.sum(params ** 2)  # Example: Standard normal
+
+            key = jax.random.PRNGKey(0)
+            prior = Normal(0, 1)
+            samples, log_densities = nuts_sample(key, log_prob_fn, prior)
+            ```
+    """
 
     def init_param_fn(seed):
         return prior.sample(seed=seed)
@@ -28,12 +82,13 @@ def nuts_sample(key, log_prob_fn, prior=None, n_samples=100_000):
     initial_states, tuned_params = jax.jit(call_warmup)(warmup_keys, init_params)
 
     def inference_loop_multiple_chains(
-        rng_key, initial_states, tuned_params, log_prob_fn, n_samples, num_chains
+        rng_key, 
+        initial_states, 
+        tuned_params, 
+        log_prob_fn, 
+        n_samples, 
+        num_chains
     ):
-        """
-            Does this just step EACH sample once? 
-            Need to run this for multiple steps?!
-        """
         kernel = blackjax.nuts.build_kernel()
 
         def step_fn(key, state, **params):
@@ -52,7 +107,12 @@ def nuts_sample(key, log_prob_fn, prior=None, n_samples=100_000):
 
     key, sample_key = jr.split(key)
     states, infos = inference_loop_multiple_chains(
-        sample_key, initial_states, tuned_params, log_prob_fn, n_samples, n_chains
+        sample_key, 
+        initial_states, 
+        tuned_params, 
+        log_prob_fn, 
+        n_samples, 
+        n_chains
     )
 
     return states.position[:, 0], states.logdensity[:, 0]
